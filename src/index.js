1631fd461f7b0766b8223fc245abc4cbacba6710
KIm, Hyeonseo <dodok8@gmail.com>
2020-08-24 00:47:56 +0900

    Refactor the whole code

--- src/index.js
+++ src/index.js
@@ -7,67 +7,75 @@ const ID = process.env.EMAIL;
 const PASSWORD = process.env.PASSWORD;
 
 const getHTML = async () => {
-    try {
-        return await axios.get(endpoint);
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    return await axios.get(endpoint);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-getHTML()
-    .then((html) => {
-        let ulList = [];
-        const $ = cheerio.load(html.data);
-        const $bodyList = $("#txt > div > div.no-more-tables > table > tbody > tr");
-        $bodyList.each(function (i, elem) {
-            ulList[i] = {
-                fixed: isNaN($(this).find("td:nth-child(1)").text()),
-                title: $(this).find("td.subject > a").text(),
-                date: new Date(Date.parse($(this).find("td:nth-child(5)").text())),
-            };
-        });
-
-        return ulList;
-    })
-    .then((ulList) => {
-        const today = new Date();
-        const currDay = 24 * 60 * 60 * 1000;
-        const criteriaDate = new Date(today.getFullYear(), today.getMonth(), today.getDate());
-        const bodyList =
-            "<ul>" +
-            ulList
-                .filter((article) => parseInt((criteriaDate - article.date) / currDay) < 5 || article.fixed)
-                .map((article) => {
-                    const tags = `<li>${article.title} ${article.date.toISOString().substring(0, 10)}</li>`;
-                    return tags;
-                })
-                .reduce((accumulator, currentValue) => accumulator + currentValue) +
-            "</ul>";
-
-        return bodyList;
-    })
-    .then((bodyList) => {
-        const today = new Date();
-        const header = `${today.getMonth() + 1}월 ${today.getDate()}일 GIST 대학 공지`;
-        const mailbody = `<h1><a href="${endpoint}">${header}</a></h1>` + "\n" + bodyList;
-        const transporter = nodemailer.createTransport({
-            service: "Outlook365",
-            auth: {
-                user: ID,
-                pass: PASSWORD,
-            },
-        });
-
-        const mailOption = {
-            from: ID,
-            to: ID,
-            subject: header,
-            html: mailbody,
-        };
-
-        try {
-            transporter.sendMail(mailOption);
-        } catch (error) {
-            console.error(error);
-        }
+const getArticles = async (now) => {
+  try {
+    const html = await getHTML();
+    let articles = [];
+    const $ = cheerio.load(html.data);
+    const $bodyList = $("#txt > div > div.no-more-tables > table > tbody > tr");
+    $bodyList.each(function (i) {
+      articles[i] = {
+        fixed: isNaN($(this).find("td:nth-child(1)").text()),
+        title: $(this).find("td.subject > a").text(),
+        date: new Date(Date.parse($(this).find("td:nth-child(5)").text())),
+      };
     });
+
+    const compareDate = (article) => {
+      return (now - article.date) / (1000 * 60 * 60 * 24) < 2;
+    };
+    const result = articles.filter(compareDate);
+    return result;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+(async function sendEmail() {
+  const now = new Date();
+  const header = `${now.getMonth() + 1}월 ${now.getDate()}일 GIST 대학 공지`;
+
+  const articles = await getArticles(now);
+
+  if (articles.length === 0) {
+    console.log("Nothing to Send");
+    return;
+  } else {
+    const bodyList =
+      "<ul>" +
+      articles
+        .map((article) => {
+          const tags = `<li>${article.title} ${article.date.toISOString().substring(0, 10)}</li>`;
+          return tags;
+        })
+        .reduce((accumulator, currentValue) => accumulator + currentValue) +
+      "</ul>";
+    const mailbody = `<h1><a href="${endpoint}">${header}</a></h1>` + "\n" + bodyList;
+    const transporter = nodemailer.createTransport({
+      service: "Outlook365",
+      auth: {
+        user: ID,
+        pass: PASSWORD,
+      },
+    });
+    const mailOption = {
+      from: ID,
+      to: ID,
+      subject: header,
+      html: mailbody,
+    };
+    try {
+      transporter.sendMail(mailOption);
+      console.log("Mail Sending Complete");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+})();
